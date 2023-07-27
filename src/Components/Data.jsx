e74d859b6efae9468c8c58b0239a8a7648f91d80
GOPIKAKB <gopikakrishnakb@gmail.com>
2023-07-27 17:24:18 +0530

    Update Data.jsx

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -3,6 +3,6 @@ const data = [{ id: 1, name: 'shoes', title: 'Lotto', descreption: 'Mens Vertigo
 { id: 3, name: 'shoes', title: 'Adidas', descreption: 'Mens Adistound M Running Shoe', price: 959, image: 'https://i.pinimg.com/originals/34/27/07/3427078fecf554d660612cad5ef2943c.jpg' },
 { id: 4, name: 'bag', title: 'DEIN KLEIDER', descreption: 'Mens Nylon Messenger Bag', price: 1908, image: 'https://media.istockphoto.com/id/156726298/photo/yellow-backpack.jpg?s=1024x1024&w=is&k=20&c=nsJTsi6CPLzJMSodyQTrJkO0s38sFozq9Vn897ki8oE=' },
 { id: 5, name: 'bag', title: 'Adisa', descreption: 'Casual School Colllage backpack', price: 2600, image: 'https://media.istockphoto.com/id/1141208525/photo/yellow-open-backpack.jpg?s=612x612&w=0&k=20&c=k0NOqN9FnIGdkaUNx6-fMIBG2IfWwLT_AbDVefqJT_0=' },
-{ id: 6, name: 'bag', title: 'American Tourister', descreption: 'Black Lapto Back Pack', price: 999,image: 'https://images.immediate.co.uk/production/volatile/sites/4/2023/02/Laptop-Backpack-Fit-15.6-inch-PC-With-USB-Charging-Plug-f923fa3.jpg?quality=90&resize=700,466'
+{ id: 6, name: 'bag', title: 'Puma', descreption: 'Black Lapto Back Pack', price: 999,image: 'https://images.immediate.co.uk/production/volatile/sites/4/2023/02/Laptop-Backpack-Fit-15.6-inch-PC-With-USB-Charging-Plug-f923fa3.jpg?quality=90&resize=700,466'
 }]
 export default data
